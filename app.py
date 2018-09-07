e66c42f9dac5e805caf98b684f3c131729ab8da1
Juan Bono <juanbono94@gmail.com>
2018-09-07 18:19:38 -0300

    add visual enhancements

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import os
-from flask import Flask, request, Response, render_template, flash, redirect, url_for
-from flask_bootstrap import Bootstrap
 import pandas as pd
-from werkzeug.utils import secure_filename
 from validation import validate
+from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
+from flask import Flask, request, Response, render_template, flash, redirect, url_for
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['csv'])
-
 APP_DIR = os.path.dirname(os.path.realpath(__name__))
 
 app = Flask(__name__)
 Bootstrap(app)
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-DEBUG = True
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def main(app):
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.run(debug=True)
 
 if __name__ == '__main__':
-    app.run(debug=DEBUG)
+    main(app)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -33,6 +34,7 @@ def put_flash(validations):
 def index():
     if request.method == 'GET':
         return render_template('index.html')
+        
     elif request.method == 'POST':
         if 'upload' not in request.files:
             flash('No file uploaded', 'danger')
